1424da3d62603acd427454ed076e5bd54423d70f
Jacob Samro <admin@jsam.in>
2017-03-16 22:29:16 +0530

    Mock wireframe added

--- public/script.js
+++ public/script.js
@@ -8,3 +8,18 @@ $("body").on("click", ".database-list li i", function(e) {
 
 
 
+var socket = io('http://localhost:8000');
+
+socket.on('connect', function(){
+
+	console.log("connected to server");
+
+});
+
+
+socket.on('get:database', function(data){
+
+	console.log(data);
+
+
+	});
--- server.js
+++ server.js
@@ -1,12 +1,57 @@
 var express = require("express");
 var app = express()
+var server = require('http').createServer(app);
+const socketio = require('socket.io')(server);
+const fs = require("fs");
 
+var PORT = 8000;
+
+var database;
+
+fs.readFile('mock.json', (err, data) => {
+
+  if (err) throw err;
+
+  var data = data.toString();
+
+  var database = JSON.parse(data);
+
+  console.log('mock database loaded');
+
+});
 
 app.use("/",express.static('public'))
 
 
-app.listen(8000,function(){
+socketio.on("connection",function (socket) {
+    
+    
+	
+	console.log("Client connected !");
+	
 
-	console.log("Server started");
+    //Emit databases names
+    socket.on("get:database",function(data){
+    	console.log("listing databases");
+		socket.emit("get:database",["db1","db2"]);
+	});
 
-})
+    //Emit table names from database given
+	socket.on("get:table",function(data){
+		//socket.emit("get:table",);
+	});
+
+
+	//Emit fields and data based on the table names from database given
+	socket.on("get:table:data",function(data){
+		//socket.emit("get:table:data",);
+	});
+
+
+
+
+});
+
+server.listen(PORT, function() {
+  console.log('Node app is running on port', PORT);
+});
